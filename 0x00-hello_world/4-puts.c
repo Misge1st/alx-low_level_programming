cf8171e976c22462f9a91f51b712fd8addef3e06
Misgana <misge1stt@gmail.com>
2023-09-08 05:49:45 -0700

    Updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
-* main - this is a main funtion
-* Return: always 0
-*/
+ * main - this is a main funtion
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
