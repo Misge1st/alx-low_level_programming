0307fe305e5f20523777f56e42195347c73a0a58
Misgana <misge1stt@gmail.com>
2023-09-08 06:18:56 -0700

    Updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,12 +12,11 @@ int main(void)
     long long int d;
     float f;
 
-    printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a)); // Corrected format specifier to %lu
-    printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b)); // Corrected format specifier to %lu
-    printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c)); // Corrected format specifier to %lu
-    printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d)); // Corrected format specifier to %lu
-    printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f)); // Corrected format specifier to %lu
-
-    return (0);
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a)); // Corrected format specifier to %lu
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b)); // Corrected format specifier to %lu
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c)); // Corrected format specifier to %lu
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d)); // Corrected format specifier to %lu
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f)); // Corrected format specifier to %lu
+return (0);
 }
                                               
