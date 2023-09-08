48deccebbf1016abaf74b56290ec620265a43210
Misgana <misge1stt@gmail.com>
2023-09-08 06:27:08 -0700

    Updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,20 @@
 #include <stdio.h> // Corrected header inclusion
-
 /**
  * main - Entry point
  * Return 0 (Success)
  */
 int main(void)
 {
-    char a;
-    int b;
-    long int c;
-    long long int d;
-    float f;
-
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a)); // Corrected format specifier to %lu
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b)); // Corrected format specifier to %lu
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c)); // Corrected format specifier to %lu
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d)); // Corrected format specifier to %lu
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f)); // Corrected format specifier to %lu
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
                                               
