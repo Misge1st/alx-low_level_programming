9e6636ff070f0a9b0bbae6bc1f010fd50b90c031
Misgana <misge1stt@gmail.com>
2023-09-08 07:41:46 -0700

    Updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using the printf f
+ * main - A program that print a line using the print
+ * 
  * Return: 0 (Success)
  */
 int main(void)
