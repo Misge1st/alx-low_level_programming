941b7d57467ac0e008c895e735c23a2a73cca049
Misgana <misge1stt@gmail.com>
2023-09-14 02:43:09 -0700

    Updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,72 @@
-#include"main.h"
+#include <stdio.h>
 
-/**
-* print_alphabet - Print the alphabet in lowercase.
-*
-* Return: void.
-*/
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
 
 void print_alphabet(void)
 {
-	int i;
+    int i;
 
-	for (i = 97; i < 123; i++)
-		{
-		_putchar(i);
-		}
-		_putchar('\n');
+    for (i = 97; i < 123; i++)
+    {
+        _putchar(i);
+    }
+    _putchar('\n');
 }
+
+void print_alphabet_x10(void)
+{
+    int i, j;
+
+    for (j = 0; j < 10; j++)
+    {
+        for (i = 97; i < 123; i++)
+        {
+            _putchar(i);
+        }
+        _putchar('\n');
+    }
+}
+
+int _abs(int n)
+{
+    if (n < 0)
+    {
+        return (-n);
+    }
+    return (n);
+}
+
+void print_to_98(int n)
+{
+    int i;
+
+    if (n <= 98)
+    {
+        for (i = n; i < 98; i++)
+        {
+            printf("%d, ", i);
+        }
+    }
+    else
+    {
+        for (i = n; i > 98; i--)
+        {
+            printf("%d, ", i);
+        }
+    }
+
+    printf("98\n");
+}
+
+int main(void)
+{
+    print_alphabet();
+    print_alphabet_x10();
+    printf("%d\n", _abs(-98));
+    print_to_98(0);
+    return (0);
+}
+
