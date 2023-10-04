4f626eb4923298554adc093e90a33272b74ae6cb
Misge1st <misge1stt@gmail.com>
2023-10-04 15:01:33 -0700

    Updated

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,87 +1,95 @@
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
+
 /**
- * **strtow - splits a string into words
- * @str: string to split
+ * alloc - allocates space in memory for an array
+ * @str: string
+ * @len: length of string
+ * @size: size of array
  *
- * Return: pointer to an array of strings (Success)
- * or NULL (Error)
+ * Return: a pointer to the array or null if allocation was unsucessful
+ */
+char **alloc(char *str, int len, int size)
+{
+	int i, j, wide;
+	char **a, before;
+
+	a = malloc((size + 1) * sizeof(char *));
+	before = ' ';
+
+	for (i = 0; i < size; i++)
+	{
+		while (j < len)
+		{
+			if (str[j] == ' ' && before != ' ')
+			{
+				before = ' ';
+				j++;
+				break;
+			}
+			if (str[j] != ' ')
+				wide++;
+			before = str[j];
+			j++;
+		}
+
+		a[i] = malloc((wide + 1) * sizeof(char));
+		if (a[i] == NULL)
+		{
+			return (NULL);
+		}
+		wide = 0;
+	}
+	a[size] = NULL;
+
+	return (a);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: the string to split
+ *
+ * Return: a pointer to an array of strings or NULL if error
  */
 char **strtow(char *str)
 {
-	int wordCount = 0;
-	int i = 0;
-	int len = strlen(str);
-	int j;
-	int start;
-	int wordIndex;
-	int wordLength;
-	char **words = NULL;
+	char **a, before = ' ';
+	int i, j = 0, k = 0, c = 0, len, size = 0;
 
-	if (str == NULL || *str == '\0')
-	{
+	if (str == NULL || strlen(str) == 0)
 		return (NULL);
-	}
-	while (i < len)
+	len = strlen(str);
+	for (i = 0; i < len; i++)
 	{
-		while (i < len && str[i] == ' ')
-		{
-			i++;
-		}
-		if (i < len && str[i] != ' ')
-		{
-			wordCount++;
-			while (i < len && str[i] != ' ')
-			{
-				i++;
-			}
-		}
+		if (str[i] != ' ')
+			c = 1;
+		if (str[i] != ' ' && before == ' ')
+			size++;
+		before = str[i];
 	}
-
-	words = (char **)malloc((wordCount + 1) * sizeof(char *));
-
-	if (words == NULL)
-	{
+	a = alloc(str, len, size);
+	if (a == NULL || c == 0)
 		return (NULL);
-	}
-
-	i = 0;
-	wordIndex = 0;
-
-	while (i < len)
+	before = ' ';
+	for (i = 0; i < size; i++)
 	{
-		while (i < len && str[i] == ' ')
+		while (j < len)
 		{
-			i++;
-		}
-		if (i < len && str[i] != ' ')
-		{
-			start = i;
-
-			while (i < len && str[i] != ' ')
+			if (str[j] == ' ' && before != ' ')
 			{
-				i++;
+				before = ' ';
+				j++;
+				break;
 			}
-			wordLength = i - start;
-
-			words[wordIndex] = (char *)malloc((wordLength + 1) * sizeof(char));
-			if (words[wordIndex] == NULL)
+			if (str[j] != ' ')
 			{
-				for (j = 0; j < wordIndex; j++)
-				{
-					free(words[j]);
-				}
-				free(words);
-				return (NULL);
+				a[i][k] = str[j];
+				k++;
 			}
-			strncpy(words[wordIndex], &str[start], wordLength);
-			words[wordIndex][wordLength] = '\0';
-			wordIndex++;
+			before = str[j];
+			j++;
 		}
+		a[i][k] = '\0';
+		k = 0;
 	}
-
-	words[wordIndex] = NULL;
-	return (words);
+	return (a);
 }
-
