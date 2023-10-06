7b7838a115cb10b271dc9bb8d8c09363302f430f
Misge1st <misge1stt@gmail.com>
2023-10-06 13:20:31 -0700

    Updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,44 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * string_nconcat - a function that concatenates two strings.
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
  *
- * @s1: first char
- * @s2: secound char
- * @n: unsigned int
- *
- * Return: If the function fails, it should return NULL
+ * Return: pointer to the memory area s
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int x, y, z;
-	char *s;
+unsigned int i;
 
-	if (s1 == NULL)
-	{
-		x = 0;
-	}
-	else
-	{
-		for (x = 0; s1[x]; ++x)
-		;
-	}
-	if (s2 == NULL)
-	{
-		y = 0;
-	}
-	else
-	{
-		for (y = 0; s2[y]; ++y)
-		;
-	}
-	if (y > n)
-		y = n;
-	s = malloc(sizeof(char) * (x + y + 1));
-	if (s == NULL)
-		return (NULL);
-	for (z = 0; z < x; z++)
-		s[z] = s1[z];
-	for (z = 0; z < y; z++)
-		s[z + x] = s2[z];
-	s[x + y] = '\0';
-	return (s);
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+
+return (s);
+}
+
+/**
+ * *_calloc - allocates memory
+ * @nmemb: number of blocks
+ * @size: the size of the allocated block
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *ptr;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+ptr = malloc(nmemb * size);
+
+if (ptr == NULL)
+exit(98);
+_memset(ptr, 0, nmemb * size);
+return (ptr);
 }
