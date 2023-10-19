ca7c7b8db672b42f671c8d7d9ffced78150e9f09
Misge1st <misge1stt@gmail.com>
2023-10-19 13:54:09 -0700

    Updaed

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -1,12 +1,11 @@
+#include "lists.h"
 #include <stdio.h>
-
+void print_something(void) __attribute__((constructor));
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_something - print before main
  */
-int main(void)
+void print_something(void)
 {
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
